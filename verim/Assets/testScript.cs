b1301bbb49ee5c5fde2930f94f15eb55a34f6ee4
Ellasy <99869506+Ellasy@users.noreply.github.com>
2022-11-17 01:58:59 +0300

    Version

---
+++ verim/Assets/testScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour
+{
+
+    public int healthPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthPoints = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
